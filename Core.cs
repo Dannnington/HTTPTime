dcefdc376e15b69d0f19eb773ebd574b72877c6a
Daniel Hyders <git@danny.works>
2021-11-08 07:06:57 +0000

    Initial

---
+++ Core.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using HTTPFetch;
+
+namespace HTTPTime
+{
+    class Core
+    {
+
+        static void GetASCII()
+        {
+            
+        }
+
+        private static async Task Main(string[] args)
+        {
+            FetchResponse TimeAPIResponse = (await Fetch.Get("https://worldtimeapi.org/api/ip"));
+            if (TimeAPIResponse.StatusCode != 200) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("There was an error with the request to the Time API. Try again later, maybe?");
+                return;
+            }
+
+            DateTime TimeAPIDate = DateTime.Parse(TimeAPIResponse.BodyAsObject.datetime);
+
+            if (TimeAPIDate.Hour < 12) {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(ASCIIArt.TimeOfDay.Morning());
+                Console.WriteLine("Good morning!");
+            } else if (TimeAPIDate.Hour < 18) {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(ASCIIArt.TimeOfDay.Noon());
+                Console.WriteLine("Good afternoon!");
+            } else {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(ASCIIArt.TimeOfDay.Evening());
+                Console.WriteLine("Good evening!");
+            }
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("The time is " + TimeAPIDate.ToString("HH:mm:ss") + ", on " + TimeAPIDate.ToString("dd MMMM yyyy") + ".");
+            Console.WriteLine("It is day " + TimeAPIResponse.BodyAsObject.day_of_year + " of this year.");
+            Console.Write("You are in the ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(TimeAPIResponse.BodyAsObject.abbreviation);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" timezone.");
+            return;
+        }
+    }
+}
---
+++ lib/httpfetch.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HTTPFetch {
+
+    /// <summary><c>FetchResponse</c> defines the properties of a response from a HTTPFetch request.</summary>
+    public sealed class FetchResponse {    
+        public String BodyAsString { get; set; } 
+        public dynamic BodyAsObject { get; set; }
+        public string url { get; set; }
+        public string ContentType { get; set; }
+        public int StatusCode { get; set; }
+        public System.Net.Http.Headers.HttpResponseHeaders Headers { get; set; }
+        public string Method { get; set; }
+    }
+
+    /// <summary><c>Fetch.Get</c> sends a HTTP GET request using the standard HTTP libraries included in .NET and Newtonsoft.Json.</summary>
+    public class Fetch {
+        static readonly HttpClient HtClient = new HttpClient();
+
+        public static async Task<FetchResponse> Get(string url) {
+            var response = await HtClient.GetAsync(url);
+
+            FetchResponse responseProperties = new FetchResponse();
+            Console.WriteLine(response.Headers.GetType().GetProperty("Content-Type"));
+            responseProperties.url = url;
+            responseProperties.ContentType = response.Content.Headers.ContentType.ToString();
+            responseProperties.Headers = response.Headers;
+            responseProperties.StatusCode = (int)response.StatusCode;
+            responseProperties.Method = "GET";
+
+            string rawResponse = await response.Content.ReadAsStringAsync();
+            responseProperties.BodyAsString = (string)rawResponse;
+            var token = Newtonsoft.Json.Linq.JToken.Parse(responseProperties.BodyAsString);
+
+            if (responseProperties.ContentType.Contains("application/json")) {
+                responseProperties.BodyAsObject = Newtonsoft.Json.JsonConvert.DeserializeObject<Types.DateTimeAPIObject>(token.ToString());
+            } else {
+                responseProperties.BodyAsObject = new Object();
+            };
+
+            return responseProperties;
+        }
+    }
+}
